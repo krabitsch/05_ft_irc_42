6d2fcf2fc9c6bae89fb580a3453c42ad09531ed3
Albert Aiyakup Ruckenbauer <aruckenb@c2r3p10.42vienna.com>
2026-01-08 11:31:20 +0100

    Rebase fixed shit

--- srcs/commandsChannel.cpp
+++ srcs/commandsChannel.cpp
@@ -14,13 +14,13 @@
     {
       if (!pass.empty())
       {
-        Channel _newchannel(this, fd,channelname, pass); //creates the channel with passkey
-        _channels.push_back(&_newchannel);
+        Channel *newchannel = new Channel(this, fd, channelname, pass); //creates the channel with passkey
+        _channels.push_back(newchannel);
       }
       else
       {
-        Channel _newchannel(this, fd,channelname); //creates the channel
-        _channels.push_back(&_newchannel);
+        Channel *newchannel = new Channel(this, fd, channelname, pass); //creates the channel
+        _channels.push_back(newchannel);
       }
       this->sendMessage(fd, _serverName, "JOIN", std::vector<std::string>(1, channelname), "You have created this channel");
     }
@@ -34,17 +34,14 @@
         return ;
       }
 
-	if (_channel == NULL)
-	{
-		Channel* newChannel = new Channel(this, fd, channelname);
-		this->_channels.push_back(newChannel);
-		_channel = newChannel;
+      //Getting Clients Channel List
+      std::map<std::string, char>*channelist = _client->GetChannel();
 
-		std::string joinMsg = ":" + _client->getNickname() + " JOIN " + channelname + "\r\n";
-		broadcastToChannel(channelname, joinMsg, -1); // broadcast to all channel mmembers (including self)
-    
+      //Checks if the user has access to the channel already or not if not add it to there channellist
+      bool checker = (channelist->find(channelname) != channelist->end());
+
       //Error Handling
-      if (_channel->getInviteonly() == true /*&& checker == false*/)
+      if (_channel->getInviteonly() == true && checker == false)
       {
         //ERR_INVITEONLYCHAN 473
         this->sendNumeric(fd, 473, "", std::vector<std::string>(), _channel->getname() + "Cannot join channel (+i)"); //You need an invite to join
@@ -67,11 +64,10 @@
       _client->setCurrentChannel(channelname);
       this->sendNotice(fd, channelname, _client->getNickname() + " has joined the channel"); 
       
-      //if (!checker) //Adds the channel to the client list !isnt is here 
+      if (!checker) //Adds the channel to the client list !isnt is here 
         _channel->AddMember(_client);
     }
   }
-}
 
   //Part //leaves the channel you are currently on!
   void Server::part(int fd, std::string channelname)
